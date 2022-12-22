b0c6f40e4f68777c0f9dbbb7e9f8f9cb6c55c121
wanghao <whateverhow@gmail.com>
2022-12-22 16:18:30 -0600

    [main]   (use "git restore --staged <file>..." to unstage)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig
